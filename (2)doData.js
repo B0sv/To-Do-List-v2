9f49d9f54723c5605820b631d6369ce81e9b9706
B0sv <48410586+B0sv@users.noreply.github.com>
2019-03-13 15:11:00 +0200

    Create (2)doData.js

---
+++ (2)doData.js
@@ -0,0 +1,29 @@
+const doData = [
+    {
+        id: 1,
+        text: "Cisco tentti, klo 12",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Käy kaupassa",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Vedä keskiviikon ryhmäliikuntatunnit",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Pese auto",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Opettele reactia",
+        completed: false
+    }
+]
+
+export default doData
